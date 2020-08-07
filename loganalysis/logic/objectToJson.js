519f19bae2077bb86e51622217b77f3a701b2956
Junyeong Jang <junyeong.dev@gmail.com>
2020-08-07 13:30:22 +0900

    Paser 부분 로직 전면 수정
    1. http access 로그에서 url 주소도 파싱할 수 있게함.
    2. 파싱한 모듈들을 counting 가능하게 함

--- loganalysis/logic/objectToJson.js
+++ loganalysis/logic/objectToJson.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 exports.objectToJson = (path, objectArray) =>{ // path 어느 경로에 저장할 것인지
     try{
-        fs.writeFileSync(path, JSON.stringify(objectArray)); 
+        fs.writeFileSync(path, JSON.stringify(objectArray, null, 4)); 
     }catch(err){
         console.log(err);
         return;
