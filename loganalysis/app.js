d2936bada11f6c50852fce3b0e815b922cac73d3
Junyeong Jang <junyeong.dev@gmail.com>
2020-08-07 13:30:43 +0900

    더미 데이터 추가

--- loganalysis/app.js
+++ loganalysis/app.js
@@ -12,6 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
 app.use(express.json());
--- loganalysis/routes/users.js
+++ loganalysis/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const { restart } = require('nodemon');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render("test.html");
 });
 
 module.exports = router;
